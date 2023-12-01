14687d7a2b9a8f1d38415d5180cd30c7aaf61a22
SNKhor <95267319+SNKhor@users.noreply.github.com>
2023-11-30 19:35:32 -0800

    Create train_layers.py

---
+++ train_layers.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.init import normal_
+
+class ConvOffset2D_train(nn.Conv2d):
+    '''
+    Convolutional layer responsible for learning the 2D offsets and output the deformed
+    feature map using bilinear interpolation
+
+    Note that this layer does not perform convolution on the deformed feature map
+
+    '''
+
+    def __init__(self, filters, init_normal_stddev=0.01, **kwargs):
+        '''
+        Parameters:
+        filters: int
+        Number of channels of the input feature map
+        init_normal_stddev: float
+        Normal kernel initialization
+        **kwargs:
+        pass to superclass. see the Conv2D layer in PyTorch
+        '''
+        self.filters = filters
+        super(ConvOffset2D_train, self).__init__(self.filters * 2, filters, kernel_size=3, padding='same', bias=False)
+        normal_(self.weight, mean=0, std=init_normal_stddev)
+
+    def forward(self, x):
+        '''
+        return the deformed featured map
+        '''
+        x_shape = x.shape
+        offsets = super(ConvOffset2D_train, self).forward(x)
+
+        # offsets: (b*c, h, w, 2)
+        offsets = self._to_bc_h_w_2(offsets, x_shape)
+        # x: (b*c, h, w)
+        x = self._to_bc_h_w(x, x_shape)
+        
+        # Assuming tf_batch_map_offsets is equivalent to F.grid_sample in PyTorch
+        x_offset = F.grid_sample(x, offsets)
+        
+        # x_offset: (b, h, w, c)
+        x_offset = self._to_b_h_w_c(x_offset, x_shape)
+
+        return x_offset
+
+    def compute_output_shape(self, input_shape):
+        '''
+        Output shape is the same as input shape
+        Because this layer only does the deformation part
+        '''
+        return input_shape
+
+    @staticmethod
+    def _to_bc_h_w_2(x, x_shape):
+        '''
+        (b, h, w, 2c)->(bc, h, w, 2)
+        '''
+        x = x.permute(0, 3, 1, 2)
+        x = x.view(-1, int(x_shape[1]), int(x_shape[2]), 2)
+        return x
+
+    @staticmethod
+    def _to_bc_h_w(x, x_shape):
+        '''
+        (b, h, w, c)->(bc, h, w)
+        '''
+        x = x.permute(0, 3, 1, 2)
+        x = x.view(-1, int(x_shape[1]), int(x_shape[2]))
+        return x
+
+    @staticmethod
+    def _to_b_h_w_c(x, x_shape):
+        '''
+        (b*c, h, w)->(b, h, w, c)
+        '''
+        x = x.view(-1, int(x_shape[3]), int(x_shape[1]), int(x_shape[2]))
+        x = x.permute(0, 2, 3, 1)
+        return x
